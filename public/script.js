53fb169547a3def13d32182920bd80208ade78ff
Boruta <56766847+RadziuMM@users.noreply.github.com>
2020-04-12 20:20:42 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -22,7 +22,7 @@ function initMap() {
   wait(2000).then(() => {
     map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 52.237049, lng: 20.017532 },
-      zoom: 4,
+      zoom: 6,
     });
     for (let i = 0; i < cordsnumber; i += 1) {
       const contentString = `${'<div id="content">'
